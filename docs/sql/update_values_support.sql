a6f491e58f3a10e0f6a5cfd93b2da448e25734b0
Gilles B <6406.number@gmail.com>
2015-04-14 17:21:00 +0200

    added sql file which update the f_support table to the db's new version

---
+++ docs/sql/update_values_support.sql
@@ -0,0 +1,23 @@
+/* Mise à jour de la table f_support */
+UPDATE f_support SET libelle="CD" WHERE support = 1;
+UPDATE f_support SET libelle="CD" WHERE support = 2;
+UPDATE f_support SET libelle="CD" WHERE support = 3;
+UPDATE f_support SET libelle="CD" WHERE support = 13;
+UPDATE f_support SET libelle='7"' WHERE support = 4;
+UPDATE f_support SET libelle='7"' WHERE support = 6;
+UPDATE f_support SET libelle="2xCD" WHERE support = 5;
+UPDATE f_support SET libelle="K7" WHERE support = 7;
+UPDATE f_support SET libelle="mini-CD" WHERE support = 8;
+UPDATE f_support SET libelle="2xLP" WHERE support = 9;
+UPDATE f_support SET libelle="LP" WHERE support = 10;
+UPDATE f_support SET libelle='12"' WHERE support = 11;
+UPDATE f_support SET libelle="n/a" WHERE support = 12;
+UPDATE f_support SET libelle="n/a" WHERE support = 14;
+
+/* Mise à jour de la table f_cd */
+UPDATE f_cd SET support = 1 WHERE support IN (SELECT support FROM f_support WHERE libelle = 'CD' AND support != 1 GROUP BY support);
+UPDATE f_cd SET support = 4 WHERE support = 6;
+UPDATE f_cd SET support = 12 WHERE support = 14;
+
+/* Suppresion des entrées de la table f_support "inutiles" */
+DELETE FROM f_support WHERE support = 2 or support = 3 or support = 13 or support = 6 or support = 14;
