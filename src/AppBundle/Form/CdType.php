d9b11f7b981021644560cdf1674f1332ea92e42c
Gilles B <6406.number@gmail.com>
2015-04-17 17:35:47 +0200

    Edition des formulaires : gestion des Entités

--- src/AppBundle/Form/CdType.php
+++ src/AppBundle/Form/CdType.php
@@ -15,18 +15,48 @@ class CdType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('artiste')
+            ->add('artiste', 'entity', array(
+                    'class' => 'AppBundle:Artiste',
+                    'property' => 'artiste'
+                ))
             ->add('titre')
-            ->add('label')
-            ->add('maison')
-            ->add('distrib')
+            ->add('label', 'entity', array(
+                    'class' => 'AppBundle:Label',
+                    'property' => 'label'
+                ))
+            ->add('maison', 'entity', array(
+                    'class' => 'AppBundle:Label',
+                    'property' => 'label'
+                ))
+            ->add('distrib', 'entity', array(
+                    'class' => 'AppBundle:Label',
+                    'property' => 'label'
+                ))
             ->add('annee')
-            ->add('type')
-            ->add('support')
-            ->add('genre')
-            ->add('langue')
-            ->add('rotation')
-            ->add('userProgra')
+            ->add('type', 'entity', array(
+                    'class' => 'AppBundle:Type',
+                    'property' => 'libelle'
+                ))
+            ->add('support', 'entity', array(
+                    'class' => 'AppBundle:Support',
+                    'property' => 'libelle'
+                ))
+            ->add('genre', 'entity', array(
+                    'class' => 'AppBundle:Genre',
+                    'property' => 'libelle'
+                ))
+            ->add('langue', 'entity', array(
+                    'class' => 'AppBundle:Langue',
+                    'property' => 'libelle'
+                ))
+            ->add('rotation', 'entity', array(
+                    'class' => 'AppBundle:Rotation',
+                    'property' => 'libelle'
+                ))
+            ->add('userProgra', 'entity', array(
+                    'class' => 'AppBundle:User',
+                    'property' => 'user'
+                ))
             ->add('dprogra')
             ->add('comment')
             ->add('jsaisie')
