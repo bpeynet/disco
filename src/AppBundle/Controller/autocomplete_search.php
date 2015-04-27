73befc729bcf3027d25da87ee12ea31ee79a5109
Gilles B <6406.number@gmail.com>
2015-04-27 17:45:40 +0200

    autocomplete raté, mais à garder

--- src/AppBundle/Controller/autocomplete_search.php
+++ src/AppBundle/Controller/autocomplete_search.php
@@ -1,26 +1,8 @@
 <?php
 
-	if(isset($_GET['type'])) {
-
-		$type= htmlspecialchars($_GET['type'])
-
-		$res = $doctrine->getRepository('AppBundle:'.$type)->findAll();
-
-		$json_ret =  array();
+	$json_ret = '[';
+	$json_ret += '{"value":"test"}';
+	$json_ret += ']';
 
 
-		foreach ($res as $key => $value) {
-			$getId = $value->get."$type";
-
-			$row = array();
-			$row['label'] = $getId;
-			$row['value'] = $value->getLibelle();
-
-			array_push($json_ret, $row);
-		}
-
-		echo json_encode($json_ret);
-
-	} else {
-		echo '';
-	}
+	echo $json_ret;
