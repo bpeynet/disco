03887681a003666353e70bdbf7a462063581c5c7
Gilles B <6406.number@gmail.com>
2015-05-27 12:32:37 +0200

    debug de genres qui n'existaient plus, et sont passés à côté

--- docs/sql/20150526alter_tables.sql
+++ docs/sql/20150526alter_tables.sql
@@ -1,2 +1,6 @@
 ALTER TABLE `f_label` CHANGE `mail_progra` `mail_progra` VARCHAR(250) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL; 
-UPDATE `f_label` SET `mail_progra` = NULL WHERE `mail_progra` = '' 
+UPDATE `f_label` SET `mail_progra` = NULL WHERE `mail_progra` = '';
+
+/*Problème au niveau des genres...*/
+
+UPDATE f_cd SET genre = NULL WHERE genre = 19 OR genre = 150;
