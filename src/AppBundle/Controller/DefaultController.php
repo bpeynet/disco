cf54d4e8a88b36e6621c98756ece20e1f34d438d
Martin Kirchgessner <martin.kirch@gmail.com>
2015-06-12 13:35:26 +0200

    fix le téléchargement de PDF

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -7,7 +7,8 @@ use AppBundle\DiscoController;
 use AppBundle\Entity\Cd;
 use AppBundle\Form\CdType;
 use Symfony\Component\HttpFoundation\Request;
-use fPDFBundle;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 
 
 class DefaultController extends DiscoController
@@ -58,37 +59,38 @@ class DefaultController extends DiscoController
 
         // //on stocke la vue à convertir en PDF, en n'oubliant pas les paramètres twig si la vue comporte des données dynamiques
         // $html = $this->renderView('default/vignettes.html.twig', array('cds' => $cds));
-         
+
         // //on instancie la classe Html2Pdf_Html2Pdf en lui passant en paramètre
         // //le sens de la page "portrait" => p ou "paysage" => l
         // //le format A4,A5...
         // //la langue du document fr,en,it...
         // $html2pdf = new \Html2Pdf('P','A4','fr');
- 
+
         // //SetDisplayMode définit la manière dont le document PDF va être affiché par l’utilisateur
         // //fullpage : affiche la page entière sur l'écran
         // //fullwidth : utilise la largeur maximum de la fenêtre
         // //real : utilise la taille réelle
         // $html2pdf->pdf->SetDisplayMode('real');
- 
+
         // //writeHTML va tout simplement prendre la vue stocker dans la variable $html pour la convertir en format PDF
         // $html2pdf->writeHTML($html);
- 
+
         // //Output envoit le document PDF au navigateur internet avec un nom spécifique qui aura un rapport avec le contenu à convertir (exemple : Facture, Règlement…)
         // $html2pdf->Output('','S');
 
-        $pdf = new FPDF();
+        $pdf = new \FPDF();
         $pdf->AddPage();
         $pdf->SetFont('Arial','B',16);
         $pdf->Cell(40,10,'Hello World!');
-        $pdf->Output();
+        $response = new Response($pdf->Output('', 'S'));
 
-        $response = new Response();
-        $response->clearHttpHeaders();
-        $response->setContent(file_get_contents($pdf));
         $response->headers->set('Content-Type', 'application/pdf');
-        $response->headers->set('Content-disposition', 'filename='. $fichier);
-         
+        $d = $response->headers->makeDisposition(
+            ResponseHeaderBag::DISPOSITION_ATTACHMENT,
+            'etiquettes.pdf'
+        );
+        $response->headers->set('Content-Disposition', $d);
+
         return $response;
     }
 
