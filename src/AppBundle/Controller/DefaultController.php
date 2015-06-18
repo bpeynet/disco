4661eed1ed7f3a5e48a0cd659e12fdac9d48427a
Martin Kirchgessner <martin.kirch@gmail.com>
2015-06-18 13:06:15 +0200

    retire l'action de test des PDF

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -39,65 +39,6 @@ class DefaultController extends DiscoController
     }
 
     /**
-     * @Route("/test/etiquettes", name="testEtiquettes")
-     */
-    public function etiquettesAction()
-    {
-        $cds = $this->getDoctrine()->getManager()->createQuery(
-                'SELECT cd
-                FROM AppBundle:Cd cd
-                ORDER BY cd.cd DESC'
-            )
-            ->setMaxResults(24)
-            ->getResult();
-
-        foreach ($cds as $key => $cd) {
-            $cd->setEtiquette(true);
-        }
-
-        $this->getDoctrine()->getManager()->flush();
-
-        // //on stocke la vue à convertir en PDF, en n'oubliant pas les paramètres twig si la vue comporte des données dynamiques
-        // $html = $this->renderView('default/vignettes.html.twig', array('cds' => $cds));
-
-        // //on instancie la classe Html2Pdf_Html2Pdf en lui passant en paramètre
-        // //le sens de la page "portrait" => p ou "paysage" => l
-        // //le format A4,A5...
-        // //la langue du document fr,en,it...
-        // $html2pdf = new \Html2Pdf('P','A4','fr');
-
-        // //SetDisplayMode définit la manière dont le document PDF va être affiché par l’utilisateur
-        // //fullpage : affiche la page entière sur l'écran
-        // //fullwidth : utilise la largeur maximum de la fenêtre
-        // //real : utilise la taille réelle
-        // $html2pdf->pdf->SetDisplayMode('real');
-
-        // //writeHTML va tout simplement prendre la vue stocker dans la variable $html pour la convertir en format PDF
-        // $html2pdf->writeHTML($html);
-
-        // //Output envoit le document PDF au navigateur internet avec un nom spécifique qui aura un rapport avec le contenu à convertir (exemple : Facture, Règlement…)
-        // $html2pdf->Output('','S');
-
-        $pdf = new \FPDF();
-        $pdf->AddPage();
-        $pdf->SetFont('Arial','B',14);
-
-        foreach ($cds as $key => $cd) {
-            $pdf->Cell(70,30,$cd->getTitre(),1);
-        }
-        $response = new Response($pdf->Output('', 'S'));
-
-        $response->headers->set('Content-Type', 'application/pdf');
-        $d = $response->headers->makeDisposition(
-            ResponseHeaderBag::DISPOSITION_ATTACHMENT,
-            'etiquettes.pdf'
-        );
-        $response->headers->set('Content-Disposition', $d);
-
-        return $response;
-    }
-
-    /**
      * @Route("/", name="index")
      */
     public function indexAction(Request $request) {
