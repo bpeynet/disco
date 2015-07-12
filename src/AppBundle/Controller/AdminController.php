febf96930bd62574970562df162bdd38b2261ed6
Martin Kirchgessner <martin.kirch@gmail.com>
2015-07-12 16:22:03 +0200

    retire /admin/encryptPass, maintenant c'est fait !

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -38,37 +38,4 @@ class AdminController extends DiscoController
             ));
     }
 
-    /**
-     * @Route("/admin/encryptPass", name="encryptPwd")
-     */
-    public function encryptPwdAction()
-    {
-        $this->denyAccessUnlessGranted('ROLE_SUPER_ADMIN', null, 'Réservé aux admins.');
-
-        $em = $this->getDoctrine()->getManager();
-
-        $users = $em->getRepository('AppBundle:User')->findAll();
-
-        foreach ($users as $key => $user) {
-            if($user->getPassword()[0] == '$') {
-                $this->addFlash('error','Un encryptage a déjà été effectué !');
-                return $this->redirect($this->generateUrl('admin'));
-            } else {
-                $plainPassword = $user->getPassword();
-                $encoder = $this->container->get('security.password_encoder');
-                $encoded = $encoder->encodePassword($user, $plainPassword);
-
-                $user->setPassword($encoded);
-                $em->persist($user);
-                $em->flush();
-            }
-        }
-
-        $this->discoLog("a chiffré les mots de passe");
-        $this->addFlash('success','Mots de passe encryptés !');
-        return $this->redirect($this->generateUrl('admin'));
-
-    }
-
 }
-
